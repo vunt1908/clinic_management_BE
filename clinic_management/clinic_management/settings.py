01f65a4f9535f89ae54a35913eef97f6c16f227d
Nguyen Tran Vu <vutran1908@gmail.com>
2025-01-08 23:36:52 +0700

    Change Setting

--- clinic_management/clinic_management/settings.py
+++ clinic_management/clinic_management/settings.py
@@ -63,7 +63,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=3),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7), 
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
